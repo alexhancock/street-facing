442112c3101159df2a005b26daa325b63c2983d1
Alex Hancock <alex@alexhancock.com>
2012-12-09 15:14:13 -0800

    [head tracking] Main logic for head tracking and shifting POV on the streetview complete

--- js/views/main.js
+++ js/views/main.js
@@ -1,38 +1,48 @@
     var Street_Facing_View = Backbone.View.extend({
         render: function() {
-            var fenway = new google.maps.LatLng(42.345573,-71.098326);
+            _.bindAll(this);
             
+            var fenway = new google.maps.LatLng(42.345573,-71.098326);
             var mapOptions = { center: fenway, zoom: 14, mapTypeId: google.maps.MapTypeId.ROADMAP };
-            var panoramaOptions = { position: fenway, pov: { heading: 270, pitch: 10, zoom: 1 }};
-
+            var panoramaOptions = { position: fenway, pov: { heading: 180, pitch: 10, zoom: 1 }};
             var map = new google.maps.Map(this.el, mapOptions);
             var panorama = new google.maps.StreetViewPanorama(this.el, panoramaOptions);
-            
             map.setStreetView(panorama);
 
             var videoInput = $('#inputVideo')[0];
             var canvasInput = $('#inputCanvas')[0];
 
-            var htracker = new headtrackr.Tracker({
-                retryDetection: false
-            });
-
+            var htracker = new headtrackr.Tracker();
             htracker.init(videoInput, canvasInput);
             htracker.start();
 
+            this.panorama = panorama;
+
+            // Throttle the calling of the handler for head movement events to once every 20ms
+            var throttled_update = _.throttle(this.handleHeadMovement, 20);
+            var pov = this.panorama.pov;
+
             $(document).on('headtrackingEvent', function(e){
-                console.log(e);
-
-                var translatedHeading = 'todo';
-                var translatedPitch = 'todo';
-
-                $(document).off('headtrackingEvent');
-                
-                /* panorama.setPov({
-                    heading: translatedHeading,
-                    pitch: translatedPitch,
-                    zoom: 0
-                }); */
+                throttled_update(pov, e);
             });
+        },
+
+        handleHeadMovement: function(currentPov, e){
+
+            // Get the amount the head moved left/right and up/down of the camera
+            var head_x = e.originalEvent.x * 2;
+            var head_y = e.originalEvent.y;
+
+            // Calculate a new heading and pitch
+            var new_heading = Math.max(0, Math.min(currentPov.heading - head_x, 360));
+            var new_pitch = currentPov.pitch - head_y;
+
+            var newPov = {
+                heading: new_heading,
+                pitch: new_pitch,
+                zoom: 0
+            };
+
+            this.panorama.setPov(newPov);
         }
     });
